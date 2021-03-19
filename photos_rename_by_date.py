0d76a57a7d62bfe70485969b1b5cfa95f8459d2a
KIRINPUTRA <seacafe@gmail.com>
2021-03-19 09:36:45 +0900

    Create photos_rename_by_date.py

---
+++ photos_rename_by_date.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+import os, sys
+
+dir = sys.argv[1]
+
+for fname in os.listdir(dir):
+    if (fname[-4:].lower() == '.jpg' or fname[-4:].lower() == '.png'):
+        print("Processing %s" % fname)
+        Fname = os.path.join(dir, fname)
+        btime_raw = os.stat(Fname).st_birthtime
+        btime = datetime.fromtimestamp(btime_raw).strftime("%Y%m%d_%I%M%S")
+        newname = os.path.join(dir, btime + fname[-4:])
+        os.rename(Fname, newname)
+print("All done")
